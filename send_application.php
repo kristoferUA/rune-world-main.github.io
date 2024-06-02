108a9112ec0999f92bb4d9b74062be3d36c9a926
kristoferUA <sukovachnikita@gmail.com>
2024-06-02 11:14:52 +0300

    Update send_application.php

--- send_application.php
+++ send_application.php
@@ -1,1 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Получение данных из формы
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
 
+    // Настройки для отправки письма
+    $to = "admruneworld@gmail.com";
+    $subject = "Новая заявка на сайте RuneWorld";
+    $message = "Имя: $name\nEmail: $email\nДискорд: $phone";
+    $headers = "From: $email";
+
+    // Отправка письма
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Заявка успешно отправлена!";
+    } else {
+        echo "Произошла ошибка при отправке заявки.";
+    }
+}
+?>
